4c61a239f96b09df4d4015bc9a52a27d5704a52d
PawXQ <pawxq@mail.com>
2026-08-21 21:52:28 +0800

    initial api interface

--- YoutubeApi/Service/Subscription.cs
+++ YoutubeApi/Service/Subscription.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YoutubeApi.Interface;
+using YoutubeApi.Model;
 
 namespace YoutubeApi.Service
 {
-    internal class Subscription
+    internal class Subscription : ISubscription
     {
+        public string URL => throw new NotImplementedException();
+
+        public Task<Model.Subscription> SubscriptionChannelAsync(string channelId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DeleteResult> UnScriptionChannelAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
